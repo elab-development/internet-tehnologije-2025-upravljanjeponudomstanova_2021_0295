fad2000ef77d27b9018d647811a08f8290c6ee4b
Petar Stojanovic <stojanovicp02@gmail.com>
2026-02-06 01:27:03 +0100

    Dodajem upite + vezu sa stanom

---
+++ backend/models/inquiry.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Inquiry extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Inquiry.init({
+    apartmentId: DataTypes.INTEGER,
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    message: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'Inquiry',
+  });
+  return Inquiry;
+};
